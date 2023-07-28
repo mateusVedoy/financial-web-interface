1a3e5bc6e5f27e2c21dfaddb96840279fab64b0d
mateusVedoy <mateusvedoy@outlook.com>
2023-07-28 20:59:55 -0300

    feat: http request to get data into transaction history

--- src/views/TransactionHistory.vue
+++ src/views/TransactionHistory.vue
@@ -1,43 +1,29 @@
 <template>
-    <TransactionHistoryBoardComponent :transactionHistoryValues="histories"/>
+  <TransactionHistoryBoardComponent :transactionHistoryValues="histories" />
 </template>
 <script>
-import TransactionHistoryBoardComponent from '@/components/TransactionHistoryBoard.vue'
+import TransactionHistoryBoardComponent from "@/components/TransactionHistoryBoard.vue";
 export default {
-    name: 'TransactionHistoryView',
-    components: {
-        TransactionHistoryBoardComponent
-    },
-    data() {
-        let histories = [];
-    },
-    methods: {
-        async getTransactionHistories() {
-            this.histories = [{
-			"id": 1,
-			"type": "input",
-			"amount": 1200.0,
-			"date": "2023-04-09",
-			"hour": "12:55:00"
-            },
-            {
-                "id": 2,
-                "type": "input",
-                "amount": 1200.0,
-                "date": "2023-04-09",
-                "hour": "12:55:00"
-            },
-            {
-                "id": 3,
-                "type": "output",
-                "amount": 2000.0,
-                "date": "2023-04-09",
-                "hour": "12:55:00"
-            }];
-        }
-    },
-    async beforeMount() {
-        await this.getTransactionHistories();
+  name: "TransactionHistoryView",
+  components: {
+    TransactionHistoryBoardComponent,
+  },
+  data() {
+    return{
+        histories: null
     }
-}
+  },
+  methods: {
+    async getTransactionHistories() {
+      const request = await fetch(
+        "http://127.0.0.1:8081/api/finances/operation/find/all"
+      );
+      const {data} = await request.json();
+      this.histories = data;
+    },
+  },
+  async beforeMount() {
+    await this.getTransactionHistories();
+  },
+};
 </script>
