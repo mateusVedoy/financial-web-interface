3628aa035f31382f3b7793eeeb54583d92d5597d
mateusVedoy <mateusvedoy@outlook.com>
2023-07-28 21:05:26 -0300

    feat: fix css style to able table rows to scroll

--- src/components/TransactionHistoryTable.vue
+++ src/components/TransactionHistoryTable.vue
@@ -1,70 +1,76 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Transfer Type</th>
-        <th>Amount</th>
-        <th>Date</th>
-        <th>Hour</th>
-      </tr>
-    </thead>
-    <tbody v-for="transaction in transactionHistoryList" :key="transaction.id">
-      <tr>
-        <td>{{transaction.id}}</td>
-        <td>{{transaction.type}}</td>
-        <td v-if="isAmountInput(transaction.type)" id="amount-green">+{{transaction.amount}}</td>
-        <td v-else id="amount-red">-{{transaction.amount}}</td>
-        <td>{{transaction.date}}</td>
-        <td>{{transaction.hour}}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Transfer Type</th>
+          <th>Amount</th>
+          <th>Date</th>
+          <th>Hour</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <div class="tbody-container">
+    <table>
+      <tbody>
+        <tr v-for="transaction in transactionHistoryList" :key="transaction.id">
+          <td style="width: 5rem">{{ transaction.id }}</td>
+          <td>{{ transaction.type }}</td>
+          <td v-if="isAmountInput(transaction.type)" id="amount-green">
+            +{{ transaction.amount }}
+          </td>
+          <td v-else id="amount-red">-{{ transaction.amount }}</td>
+          <td>{{ transaction.date }}</td>
+          <td>{{ transaction.hour }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
 export default {
   name: "TransactionHistoryTableComponent",
   props: {
     transactionHistoryList: {
-      type: Object
+      type: Object,
     },
   },
   methods: {
     isAmountInput(value) {
-        return value === "input";
-    }
-  }
+      return value === "input";
+    },
+  },
 };
 </script>
 <style scoped>
 table {
   border-collapse: collapse;
-  margin: 2rem 0;
+  margin: 0.8rem 0;
   font-family: "Courier New", Courier, monospace;
   font-size: 1rem;
   min-width: 100%;
   color: #525151;
 }
-table thead tr {
+.tbody-container {
+  max-height: 20rem;
+  overflow-x: scroll;
+}
+.tbody-container tr {
   text-align: center;
 }
-table thead th {
-  margin-left: 5rem;
-}
-table tbody tr {
-  text-align: center;
-}
-th,
-td {
-  padding: 0.8rem 0.8rem;
+th, td {
+  padding: .8rem;
+  width: 5rem;
 }
 table tbody tr:hover {
-    background-color: #fafafa;
+  background-color: #fafafa;
 }
 #amount-green {
-    color: #329B01;
+  color: #329b01;
 }
 #amount-red {
-    color: #B64040;
+  color: #b64040;
 }
 </style>
